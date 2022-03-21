4872e844640825cb10ee9f0548256607ee77d0ba
mahrokh sadro <msadrolodabaee@myseneca.ca>
2022-03-21 18:00:44 -0400

    first commit

---
+++ src/payable/Employee.java
@@ -0,0 +1,40 @@
+package payable;
+
+public class Employee implements Payable{
+
+	String firstName;
+	String lastName;
+	String ssn;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public Employee(String first, String last, String ssn) {
+		this.firstName=first;
+		this.lastName=last;
+		this.ssn=ssn;
+	}
+	@Override
+	public String toString() {
+		return "FirstName : " + getFirstName() + " | LastName : " + getLastName() + " | Ssn : "
+				+ getSsn() + "\n";
+	}
+	
+	
+}
---
+++ src/payable/Invoice.java
@@ -0,0 +1,68 @@
+package payable;
+
+public class Invoice implements Payable{
+	
+	String part;
+	String description;
+	int count;
+	double price;
+	
+	public Invoice(String part, String description, int count, double price) {
+		this.part=part;
+		this.description=description;
+		this.count=count;
+		this.price=price;
+	}
+	
+	String getPart() {
+		return part;
+	}
+	
+	void setPart(String part) {
+		this.part=part;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Part : "+getPart()+ " | Description : "+getDescription()+" | Count : "
+	                 +getCount()+" | Price : "+getPrice()+"\n";
+	}
+
+	@Override
+	
+	public double getPaymentAmount() {
+		
+		return getCount()*getPrice();
+	}
+	
+	
+	
+
+	
+	
+	
+}
